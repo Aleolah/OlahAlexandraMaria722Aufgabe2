70d79450958b3f32449f84280d7229dc922850fc
Aleolah <alexandra.olah@stud.ubbcluj.ro>
2025-01-15 15:33:50 +0200

    Controller crud operations

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,6 +1,12 @@
 package Controller;
 
 
+import Model.Medikament;
+import Model.Patienten;
+import Repository.Repository;
+
+import java.util.ArrayList;
+
 /**
  * The `Controller` class is the central component of the application's logic.
  * It is responsible for handling and processing the user's input from the console,
@@ -8,37 +14,70 @@ package Controller;
  * removing, and updating entities, and displaying information.
  */
 public class Controller {
+    Repository<Medikament> medikamentRepository;
+    Repository<Patienten> patientenRepository;
 
-    public Controller() {
-
+    public Controller(Repository<Medikament> medikamentRepository, Repository<Patienten> patientenRepository) {
+        this.medikamentRepository = medikamentRepository;
+        this.patientenRepository = patientenRepository;
     }
 
     /**
      * Adds a new item.
      */
-    public void add(){
+    public void addMedikament(String name, int preis, String krankheit){
+        Medikament med = new Medikament(name, preis, krankheit);
+        medikamentRepository.addItem(med);
 
     }
 
     /**
      * Deletes a
      */
-    public void delete(){
-
+    public void deleteMedikament(String name){
+        medikamentRepository.deleteItem(medikament -> medikament.getName().equals(name));
     }
 
     /**
      * Updates a
      */
-    public void update(){
-
+    public void updateMedikament(String oldname, String name, int preis, String krankheit){
+    medikamentRepository.updateItem(medikament -> medikament.getName().equals(oldname),new Medikament(name, preis, krankheit));
     }
 
     /**
      * Displays all
      */
-    public void display(){
+    public void displayMedikaments(){
+    medikamentRepository.getAllItems();
+    }
+    /**
+     * Adds a new item.
+     */
+    public void addPatient(int id, String name, int age, String diagnose, ArrayList<Medikament> medikament){
+        patientenRepository.addItem(new Patienten(id,name,age,diagnose,medikament));
 
+    }
+
+    /**
+     * Deletes a
+     */
+    public void deletePatient(int id){
+        patientenRepository.deleteItem(patienten -> patienten.getId() == id);
+    }
+
+    /**
+     * Updates a
+     */
+    public void updatePatient(int oldId, int id, String name, int age, String diagnose, ArrayList<Medikament> medikament){
+        patientenRepository.updateItem(patienten -> patienten.getId() == oldId,new Patienten(id,name,age,diagnose,medikament));
+    }
+
+    /**
+     * Displays all
+     */
+    public void displayPatienten(){
+        patientenRepository.getAllItems();
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 import Console.Console;
 import Controller.Controller;
+import Model.Medikament;
+import Model.Patienten;
+import Repository.Repository;
 
 public class Main {
     public static void main(String[] args) {
-        Controller controller = new Controller();
+        Repository<Medikament> medikamentRepository = new Repository();
+        Repository<Patienten> patientenRepository = new Repository();
+
+        Controller controller = new Controller(medikamentRepository, patientenRepository);
         Console console = new Console(controller);
         console.run();
     }
